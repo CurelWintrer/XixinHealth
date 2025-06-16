2f9bc307ed6818c0633513711411b2b730816392
kali <b1131683978@163.com>
2025-06-17 00:30:28 +0800

    Add login functionality: implement user login by userId and password

--- backend/src/main/java/com/neusoft/tijian/TijianServerRunApplication.java
+++ backend/src/main/java/com/neusoft/tijian/TijianServerRunApplication.java
@@ -2,9 +2,8 @@ package com.neusoft.tijian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class TijianServerRunApplication {
     public static void main(String[] args) {
         SpringApplication.run(TijianServerRunApplication.class,args);
---
+++ backend/src/main/java/com/neusoft/tijian/service/UsersService.java
@@ -0,0 +1,8 @@
+package com.neusoft.tijian.service;
+
+import com.neusoft.tijian.po.Users;
+
+
+public interface UsersService {
+    Users getUsersByUserIdByPass(Users users);
+}
