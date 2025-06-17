c117b485e418a53b0d7551c50604f77dd17e9f25
kali <b1131683978@163.com>
2025-06-17 19:24:27 +0800

    Update OrdersMapper

--- backend/src/main/java/com/neusoft/tijian/mapper/OrdersMapper.java
+++ backend/src/main/java/com/neusoft/tijian/mapper/OrdersMapper.java
@@ -3,6 +3,8 @@ package com.neusoft.tijian.mapper;
 import com.neusoft.tijian.dto.CalendarRequestDto;
 import com.neusoft.tijian.dto.CalendarResponseDto;
 import com.neusoft.tijian.dto.OrdersMapperDto;
+import com.neusoft.tijian.po.Orders;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -18,6 +20,8 @@ public interface OrdersMapper {
     //根据parameList参数 查询30天的预约日期中 每一天的已预约人数
     List<CalendarResponseDto> listOrdersAppointmentNumber(List<OrdersMapperDto> list);
 
-
+    //创建体检预约订单
+    @Insert("insert into orders values(null,#{orderDate},#{userId},#{hpId},#{smId},1)")
+    int saveOrders(Orders orders);
 
 }
--- backend/src/main/java/com/neusoft/tijian/service/OrdersService.java
+++ backend/src/main/java/com/neusoft/tijian/service/OrdersService.java
@@ -1,5 +1,8 @@
 package com.neusoft.tijian.service;
 
+import com.neusoft.tijian.po.Orders;
+
 public interface OrdersService {
     int getOrdersByUserId(String userId);
+    int saveOrders(Orders orders);
 }
